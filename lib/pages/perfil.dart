91aedb9ec66fdf69dc38302f7af5332a1e848518
carlos leon <cleon050591@gmail.com>
2024-06-23 00:27:31 -0500

    init

--- lib/pages/perfil.dart
+++ lib/pages/perfil.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:futuresflutter/models/pendiente_model.dart';
 import 'package:futuresflutter/models/profile_model.dart';
 import 'package:futuresflutter/models/social_model.dart';
